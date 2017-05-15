71d4f91cfcaaf7251e3d528e495cc907d672d64b
sourish.sarkar <sourish.sarkar@accenture.com>
2017-05-15 19:52:37 +0530

    First commit

---
+++ StringFormatters.py
@@ -0,0 +1,18 @@
+'''
+Created on May 7, 2017
+
+@author: sourish.sarkar
+'''
+s1='apples'
+s2="oranges"
+s5="John likes {},{}".format(s1,s2)
+print("s5" ,s5)
+#Positional arguments
+s6="John likes {1},{0}, {1}".format(s1,s2)
+print("s6:",s6)
+#Keyword arguments
+s7="John likes {app},{ora},{app}".format(app=s1.capitalize(),ora=s2.swapcase())
+print("s7:",s7)
+#Keyword and Positional arguments can be used together
+s8="John likes {ora},{0}".format(s1,ora=s2)
+print("s8:",s8)

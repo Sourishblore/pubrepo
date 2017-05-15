bb072d5b8def53bad012dd5113c6fa6924bda226
sourish.sarkar <sourish.sarkar@accenture.com>
2017-05-15 20:02:39 +0530

    File updated

--- StringFormatters.py
+++ StringFormatters.py
@@ -5,7 +5,8 @@ Created on May 7, 2017
 '''
 s1='apples'
 s2="oranges"
-s5="John likes {},{}".format(s1,s2)
+s3="grapes"
+s5="John likes {},{},{}".format(s1,s2,s3)
 print("s5" ,s5)
 #Positional arguments
 s6="John likes {1},{0}, {1}".format(s1,s2)
